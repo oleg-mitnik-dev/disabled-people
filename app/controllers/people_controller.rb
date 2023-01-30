28bcd6959c1b2f6a1b03bd366e893cd8c6f72711
Oleg Mitnik <olegmitnik.dev@gmail.com>
2023-01-30 11:16:44 +0300

    Add people#show action
    Added people#show action and routes.rb refactored with recources

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -2,4 +2,8 @@ class PeopleController < ApplicationController
   def index
     @people = Person.all
   end
+
+  def show
+    @person = Person.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   root "people#index"
-  get "/people", to: "people#index"
+  resources :people
 end
