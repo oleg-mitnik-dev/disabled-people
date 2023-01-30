36c1496ff3bd7f43e421ec91245075fbdde84c17
Oleg Mitnik <olegmitnik.dev@gmail.com>
2023-01-30 12:12:22 +0300

    Create people#create action

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -10,4 +10,19 @@ class PeopleController < ApplicationController
   def new
     @person = Person.new
   end
+
+  def create
+    @person = Person.new(person_params)
+    if @person.save 
+      redirect_to @person
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def person_params
+    params.require(:person).permit(:name, :surname)
+  end
 end
