3406c13f2a7c22810863616ec958b2e7704e2f64
Oleg Mitnik <olegmitnik.dev@gmail.com>
2023-01-31 09:18:10 +0300

    Refactor validation error messages

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,4 +1,4 @@
 class Person < ApplicationRecord
-  validates :name, presence: true
-  validates :surname, presence: true
+  validates :name, presence: true, length: { minimum: 3 }
+  validates :surname, presence: true, length: {minimum: 5 }
 end
