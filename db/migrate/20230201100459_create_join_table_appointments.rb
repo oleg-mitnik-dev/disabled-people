95e8b777952a4ce9f33eee6b3cbe5a1a1e72fbe9
Oleg Mitnik <olegmitnik.dev@gmail.com>
2023-02-01 13:05:34 +0300

    Create join table Appointments

---
+++ db/migrate/20230201100459_create_join_table_appointments.rb
@@ -0,0 +1,8 @@
+class CreateJoinTableAppointments < ActiveRecord::Migration[7.0]
+  def change
+    create_join_table :people, :guardians do |t|
+      # t.index [:person_id, :guardian_id]
+      # t.index [:guardian_id, :person_id]
+    end
+  end
+end
