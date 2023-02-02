a660a420efa58765e34b7d86b6306e76bd81cf7f
Oleg Mitnik <olegmitnik.dev@gmail.com>
2023-02-02 13:05:52 +0300

    Add guardings#create action and form for creating new Guardian

--- app/controllers/guardians_controller.rb
+++ app/controllers/guardians_controller.rb
@@ -1,2 +1,15 @@
 class GuardiansController < ApplicationController
+  def create
+    @person = Person.find(params[:person_id])
+    @guardian = @person.guardians.create(guardian_params)
+    redirect_to person_path(@person)
+  end
+
+  private
+
+  def guardian_params
+    #<ActionController::Parameters {... ,"guardian"=>{"name"=>"Guardian Name"}, ...}
+    params.require(:guardian).permit(:name)
+  end
+
 end
