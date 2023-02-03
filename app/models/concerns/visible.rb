8a6d68e44c9fc7e4bc0e94e6494dd24eb6fda410
Oleg Mitnik <olegmitnik.dev@gmail.com>
2023-02-03 15:44:26 +0300

    Create concern Visible

---
+++ app/models/concerns/visible.rb
@@ -0,0 +1,15 @@
+module Visible
+  extend ActiveSupport::Concern
+
+  VALID_STATUSES = ['public', 'private', 'archived']
+
+  <%# The code inside the included block is evaluated in the context %>
+  <%# of the including class.  %>
+  included  do
+    validates :status, inclusion: { in: VALID_STATUSES }
+  end
+
+  def archived?
+    status == 'archived'
+  end
+end
