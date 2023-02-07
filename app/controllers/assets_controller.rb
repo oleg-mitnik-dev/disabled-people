08718ca2d919ffb5fb55c95784e4f6a729bc04b9
Oleg Mitnik <olegmitnik.dev@gmail.com>
2023-02-07 16:08:11 +0300

    Generate controller Assets

---
+++ app/controllers/assets_controller.rb
@@ -0,0 +1,7 @@
+class AssetsController < ApplicationController
+  def create
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'assets/create'
+  get 'assets/destroy'
   root "people#index"
   resources :people do
     resources :guardians
